224b34c782639f4d0681555a5db1bc5d18120239
GutzC <camdengutzman@gmail.com>
2015-01-12 09:01:58 -0700

    Adding wording of problem one

--- Toy_Problems.js
+++ Toy_Problems.js
@@ -1,5 +1,7 @@
 /**
- * Created by cglapto on 1/12/2015.
+ * Problem One
+ * Write a function called finder that accepts two parameters, the first one is a string and the second is an array.
+ * Have your function return true if the string is found in the array and false if i is not.
  */
 var finder = function(word, collection){
     if(collection.indexOf(word) !== -1){
