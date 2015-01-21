7f9bc761c74575b16d89c9c192ebbefbcf543301
GutzC <camdengutzman@gmail.com>
2015-01-21 09:45:19 -0700

    Added string/symbol search

--- Toy_Problems.js
+++ Toy_Problems.js
@@ -198,3 +198,22 @@ var factorial = function(num){
     }
     return num * factorial(num - 1);
 }
+
+/*
+ Write a function called simpleSymbols that takes in a string parameter and determines if it is an acceptable sequence by
+ either returning the string true or false. The str parameter will be composed of + and = symbols with several letters
+ between them (ie. ++d+===+c++==a) and for the string to be true each letter must be surrounded by a + symbol. So the string
+ to the left would be false. The string will not be empty and will have at least oe letter.
+*/
+
+var simpleSymbols = function(string){
+    // Search through string until a letter is found
+    for(var i = 0; i < string.length; i++){
+        if(string[i] !== '+' && string[i] !== '='){
+            if (string[i - 1] !== '+' || string[i + 1] !== '+') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
