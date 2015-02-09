b64108afc061a2e5b0aec8962353347706e1e469
GutzC <camdengutzman@gmail.com>
2015-02-09 09:35:53 -0700

    Counting occurences of nested object keys

--- Toy_Problems.js
+++ Toy_Problems.js
@@ -379,3 +379,125 @@ var flatten = function(arr){
     }
     return newArr;
 }
+
+//given the above object, write a function that returns an object whose keys are the fields found below and whose values are objects containing all the values as keys and a count for how many times that value occurs
+
+//e.g.
+//{
+//    kingdom: {Animalia: 6}
+//    phylum: {Chordata: 6},
+//    class: {Mammalia: 4, Reptilia: 1, Aves: 1}
+//}
+
+var animals = [
+    {
+        name: 'Dog',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Carnivora',
+        family: 'Canidae',
+        species: 'Canis lupus'
+    },
+    {
+        name: 'Cat',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Carnivora',
+        family: 'Felidae',
+        species: 'Felis catus'
+    },
+    {
+        name: 'Hippopotamus',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Artiodactyla',
+        family: 'Hippopotamidae',
+        species: 'Hippopotamus amphibius'
+    },
+    {
+        name: 'Playtpus',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Monotremata',
+        family: 'Ornithorhynchidae',
+        species: 'Ornithorhynchus anatinus'
+    },
+    {
+        name: 'Lizard',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Reptilia',
+        order: 'Squamata',
+        family: 'Agamidae',
+        species: 'Pogona vitticeps'
+    },
+    {
+        name: 'Blackbird',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Aves',
+        order: 'Passeriformes',
+        family: 'Turdidae',
+        species: 'Turdus merula'
+    }
+];
+
+//  { kingdom: {Animalia: 6}
+//    phylum: {Chordata: 6},
+//    class: {Mammalia: 4, Reptilia: 1, Aves: 1
+//  }
+
+
+//code here
+
+var getClassification = function(arr){
+    newObj = {
+        name: {},
+        kingdom: {},
+        phylum: {},
+        class: {},
+        order: {},
+        family: {},
+        species: {}
+    };
+    for(var i = 0; i < arr.length; i++) {
+        // Loop through each object in array
+        for (var key in arr[i]) {
+            if(arr[i].hasOwnProperty(key)) {
+                debugger;
+                // If the key isn't found
+                if (newObj.key[arr[i].key]) {
+                    newObj.key.arr[i] = 0;
+                } else {
+                    newObj.key[arr[i].key]++;
+                }
+            }
+        }
+    }
+    return newObj;
+}
+
+
+// Cahlan's solution
+//var breakDown = function(items) {
+//    var b = {};
+//    for(var i = 0; i<items.length; i++) {
+//        for(var prop in items[i]) {
+//            if(!b[prop]) {
+//                b[prop] = {};
+//            }
+//            if (!b[prop][items[i][prop]]) {
+//                b[prop][items[i][prop]] = 1;
+//            }
+//            else {
+//                b[prop][items[i][prop]]++;
+//            }
+//        }
+//    }
+//    return b;
+//}
+// check if value for each key in object is already in the new object
